fafb1d2cd5fca302f24c999f4ce481b8e1d1a6c9
vaidya0303 <vaidya0304@gmail.com>
2022-11-13 19:31:07 +0530

    Added: edit existing contact person using their name UC3

--- src/com/day27/AddressBookSystem.java
+++ src/com/day27/AddressBookSystem.java
@@ -4,7 +4,7 @@ package com.day27;
  * UC1 :- Ability to create a Contacts in Address Book with first and last names, address,
  *        city, state, zip, phone number and email...
  * UC2 :- Ability to add a new Contact to Address Book
- *
+ * UC3 :- Ability to edit existing contact person using their name
  */
 
 /**
@@ -28,7 +28,7 @@ public class AddressBookSystem{
      * The Scanner class is used to get user input, and it is found in the java.util package.
      * create a scanner class object
      */
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     /**
      * This method is used to add details to address book
@@ -57,14 +57,83 @@ public class AddressBookSystem{
         System.out.println("Enter the phone number");
         info.setPhoneNumber(sc.nextLong());
         arrayDetails.add(info);
-        sc.close();
+//        sc.close();
     }
-
     /**
-     * This method is used to display the added information
+     * create a method name as editDetails.
+     * This method is used to edit the details in address book
      */
-    public void display() {
-        System.out.println(arrayDetails);
+
+    public void editDetails() {
+        System.out.println("Confirm your first name to edit details: ");
+        String confirmName = sc.next();
+        /**
+         * using for loop
+         * a)i=0=initialize; b)i < arrayDetails.size()=condition; c)i++= increment by 1
+         *
+         * 1) 1st initialize the value of a variable,then check the condition,then go to if condiion,in this condition arrayDetails object
+         *   the element at the specified position in this list then if they are equals then select the option
+         *   choose edit detatils then 1st confirm ur first name to edit details then put ur previous name what u set this,
+         *   then select what u want change,suppose u choose first name,
+         *   then put it new name what u want then its edit and after the print all the information with editing new name
+         *   then again update.
+         *
+         */
+        for (int i = 0; i < arrayDetails.size(); i++) {
+            if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
+                System.out.println("Select form below to change: ");
+                System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
+                int edit = sc.nextInt();
+                sc.close();
+
+                /**
+                 * switch case is used for what u want edit
+                 */
+                switch (edit) {
+                    case 1:
+                        System.out.println("Enter first name");
+                        arrayDetails.get(i).setFirstName(sc.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter Last name");
+                        arrayDetails.get(i).setLastName(sc.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter Address");
+                        arrayDetails.get(i).setAddress(sc.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter City");
+                        arrayDetails.get(i).setCity(sc.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter State");
+                        arrayDetails.get(i).setState(sc.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter Zip");
+                        arrayDetails.get(i).setZip(sc.nextInt());
+                        break;
+                    case 7:
+                        System.out.println("Enter Mobile number");
+                        arrayDetails.get(i).setPhoneNumber(sc.nextLong());
+                        break;
+                    case 8:
+                        System.out.println("Enter new E-mail");
+                        arrayDetails.get(i).setEmail(sc.next());
+                        break;
+                }
+                /**
+                 * if you will check the name which you have written earlier,whether it matches or not ,
+                 * if it matches then it will be edited after that.
+                 * if  you put previous name is incorrect then show the else statement
+                 */
+                System.out.println("Edited list is: ");
+                System.out.println(arrayDetails);
+            } else
+                System.out.println("Enter a valid First name");
+        }
+
     }
 
     /**
@@ -72,10 +141,6 @@ public class AddressBookSystem{
      * @param args no arguments
      */
     public static void main(String[] args) {
-        /**
-         * 1st print the welcome msg.
-         */
-        System.out.println("Welcome to Address Book Program");
         /**
          * create a object name as details for  AddressBookSystem  class
          * create object bcoz AddressBookSystem in this class all method is non static
@@ -88,6 +153,29 @@ public class AddressBookSystem{
          * method=addDetails(),display()
          */
         details.addDetails();
-        details.display();
+
+        int i = 0;
+        while (i == 0) {
+            /**
+             * 1st print the welcome msg.
+             */
+            System.out.println("Welcome to Address Book Program");
+            System.out.println("What do you want to do: ");
+            System.out.println("1.Add details.\n2.Edit details.");
+            int choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    details.addDetails();
+                    break;
+                case 2:
+                    details.editDetails();
+                    break;
+                default:
+                    i = 1;
+                    System.out.println("Wrong option");
+                    break;
+            }
+        }
+
     }
 }
